6fd09a3ded1c4c6007025a591f98a234ca15d4ae
Lautaro Monteys <lmonteys@dc.uba.ar>
2023-10-15 12:32:00 -0300

    Ejercicio 3.1, 3.2 y 3.3

---
+++ ego1-2.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+feat = np.loadtxt("..//ego-facebook.feat")[:, 1:]  # Sin el primer atributo porque es el id del nodo
+feat_with_first = np.loadtxt("..//ego-facebook.feat")
+edges = np.loadtxt("..//ego-facebook.edges", dtype=int)
+
+
+# Mapear ids de nodo a índices de matriz
+edge_map = {}
+for i, line in enumerate(feat_with_first):
+    edge_map[line[0]] = i
+
+
+similarity_matrix = feat @ feat.T
+
+# Acá tomé el percentil 90 por tomar uno cualquiera
+percentile_similarity = np.percentile(similarity_matrix, 90)
+
+G = nx.Graph()
+for i in range(feat.shape[0]):
+    G.add_node(i)
+for i in range(similarity_matrix.shape[0]):
+    for j in range(i+1, similarity_matrix.shape[1]):
+        if similarity_matrix[i,j] > percentile_similarity:
+            G.add_edge(i, j)
+nx.draw(G, node_size=10)
+plt.show()
+
+# Correlación de matrices de adyacencia aplanadas
+adjacency_matrix = np.zeros((feat.shape[0], feat.shape[0]))
+for i, j in edges:
+    i = edge_map[i]
+    j = edge_map[j]
+    adjacency_matrix[i, j] = 1
+    adjacency_matrix[j, i] = 1
+
+original_flattened = adjacency_matrix.flatten()
+nuestra_flattened = nx.adjacency_matrix(G).todense().flatten()
+
+correlation = np.corrcoef(original_flattened, nuestra_flattened)[0,1] # ¿¿¿A esto se refieren con "correlación"??? ¿O a producto interno?
+print(f'Correlación de matrices de adyacencia aplanadas: {correlation}')
+
+
+
+# Correlación de listas de autovalores
+eigenvalues, _ = np.linalg.eig(adjacency_matrix)
+eigenvalues_original = np.linalg.eigvals(adjacency_matrix)
+correlation = abs(np.corrcoef(eigenvalues, eigenvalues_original)[0,1])  # ¿¿¿A esto se refieren con "correlación"??? ¿O a producto interno?
+                                                                        # Le puse abs porque me estaba dando un número complejo
+print(f'Correlación de listas de autovalores: {correlation}')
---
+++ ego3.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+feat = np.loadtxt("..//ego-facebook.feat")[:, 1:]  # Sin el primer atributo porque es el id del nodo
+feat_with_first = np.loadtxt("..//ego-facebook.feat")
+edges = np.loadtxt("..//ego-facebook.edges", dtype=int)
+
+# Mapear ids de nodo a índices de matriz
+edge_map = {}
+for i, line in enumerate(feat_with_first):
+    edge_map[line[0]] = i
+
+similarity_matrix = feat @ feat.T
+
+# Vamos a hacer el calculo de correlacion con los dos métodos: aplanando la matriz de adyacencia y con los autovalores
+correlations_flat = []
+correlations_eigenvalues = []
+
+# Creamos la matriz de adyacencia original
+adjacency_matrix = np.zeros((feat.shape[0], feat.shape[0]))
+for i, j in edges:
+    i = edge_map[i]
+    j = edge_map[j]
+    adjacency_matrix[i, j] = 1
+    adjacency_matrix[j, i] = 1
+original_flattened = adjacency_matrix.flatten()
+
+percentiles_to_test = np.arange(0, 100, 1)
+
+for percentile_similarity in percentiles_to_test:
+    G = nx.Graph()
+    for i in range(feat.shape[0]):
+        G.add_node(i)
+    similarity_threshold = np.percentile(similarity_matrix, percentile_similarity)
+    for i in range(similarity_matrix.shape[0]):
+        for j in range(i+1, similarity_matrix.shape[1]):
+            if similarity_matrix[i,j] > similarity_threshold:
+                G.add_edge(i, j)
+
+    eigenvalues, eigenvectors = np.linalg.eig(nx.adjacency_matrix(G).todense())
+
+    
+    # Correlación de listas de autovalores
+    eigenvalues_original = np.linalg.eigvals(adjacency_matrix)
+    correlation_eigenvalues = abs(np.corrcoef(eigenvalues, eigenvalues_original)[0,1])
+    correlations_eigenvalues.append(correlation_eigenvalues)
+
+    # Correlación de matrices de adyacencia aplanadas
+    nuestra_flattened = nx.adjacency_matrix(G).todense().flatten()
+    correlation_flat = np.corrcoef(original_flattened, nuestra_flattened)[0,1]
+    correlations_flat.append(correlation_flat)
+    print(f'Percentil: {percentile_similarity}, correlación flat: {correlation_flat}, correlación eigenvalues: {correlation_eigenvalues}')
+
+# Printear maximos
+for correlations in [correlations_flat, correlations_eigenvalues]:
+    max_correlation = max(correlations)
+    max_percentile = np.argmax(correlations)
+    print(f'Max correlation: {max_correlation}')
+    print(f'Max percentile: {max_percentile}')
+
+# Graficar maximos
+for correlations in [correlations_flat, correlations_eigenvalues]:
+    plt.plot(percentiles_to_test, correlations)
+    plt.axvline(x=percentiles_to_test[np.argmax(correlations)], color='r', linestyle='--', label=f'Percentil {np.argmax(correlations)}')    
+    plt.xlabel('Umbral de similaridad (percentil)')
+    plt.ylabel('Correlación')
+    plt.legend()
+    plt.show()
