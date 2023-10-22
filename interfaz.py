52b22391f805609bfabc55624ac7f424b899bed0
MartinSztajn <92404960+MartinSztajn@users.noreply.github.com>
2023-10-22 15:55:41 -0300

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -1,20 +1,29 @@
-from IPython.core.hooks import subprocess
-import subprocess
 import numpy as np
+import subprocess
 
 A = np.array([[1, 2, 3], [2, 5, 6], [3, 6, 9]], dtype=np.float64)
-!rm input_data.txt
-with open('input_data.txt','a') as f:
-   f.write(f"{A.shape[0]} {A.shape[1]}\n")
-   np.savetxt(f,A, newline="\n")
-   f.write(f"{1e-12}")
+
+# Crear el archivo input_data.txt y escribir los datos
+with open('input_data.txt', 'w') as f:
+    f.write(f"{A.shape[0]} {A.shape[1]}\n")
+    np.savetxt(f, A, newline="\n")
+    f.write(f"{1e-12}")
+
+# Crear el archivo autovalores.txt y autovectores.txt como un archivo vacío
+with open('autovalores.txt', 'w') as f:
+    pass
+with open('autovectores.txt', 'w') as f:
+    pass
 
 
-!rm eigen_ctypes_test.so #borramos por las dudas
-!g++ -shared -fPIC -Llibdl -o eigen_ctypes_test.so Potencia+Deflacion.c++
-!ls #chequeamos que este la lib
-!pwd #nos fijamos en que carpeta esta
+# Compila el archivo C++ utilizando g++
+compilation_command = f'g++ potenciadeflacion.cpp -o metodopotenciadeflacion
+subprocess.run(compilation_command, shell=True, check=True)
 
-argv = ["/content/eigen_ctypes_test.so", "./out", "input_data.txt", "output_data.txt"]
-subprocess.run(argv)
+# Ejecuta el programa C++
+cpp_program = './metodopotenciadeflacion
+subprocess.run(cpp_program, shell=True)
+
+autovalores = np.loadtxt('autovalores.txt')
+autovectores = np.loadtxt('autovectores.txt')
 
