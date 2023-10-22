821d2dfa43b357954ebfe670cc948f519b8f0aa3
MartinSztajn <92404960+MartinSztajn@users.noreply.github.com>
2023-10-22 18:32:57 -0300

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -1,29 +1,31 @@
 import numpy as np
 import subprocess
 
-A = np.array([[1, 2, 3], [2, 5, 6], [3, 6, 9]], dtype=np.float64)
 
-# Crear el archivo input_data.txt y escribir los datos
-with open('input_data.txt', 'w') as f:
-    f.write(f"{A.shape[0]} {A.shape[1]}\n")
-    np.savetxt(f, A, newline="\n")
-    f.write(f"{1e-12}")
+def potenciadeflacion(A):
+  # Crear el archivo input_data.txt y escribir los datos
+  with open('input_data.txt', 'w') as f:
+      f.write(f"{A.shape[0]} {A.shape[1]}\n")
+      np.savetxt(f, A, newline="\n")
+      f.write(f"{1e-12}")
 
-# Crear el archivo autovalores.txt y autovectores.txt como un archivo vacío
-with open('autovalores.txt', 'w') as f:
-    pass
-with open('autovectores.txt', 'w') as f:
-    pass
+  # Crear el archivo autovalores.txt y autovectores.txt como un archivo vacío
+  with open('autovalores.txt', 'w') as f:
+      pass
+  with open('autovectores.txt', 'w') as f:
+      pass
 
 
-# Compila el archivo C++ utilizando g++
-compilation_command = f'g++ potenciadeflacion.cpp -o metodopotenciadeflacion
-subprocess.run(compilation_command, shell=True, check=True)
+  # Compila el archivo C++ utilizando g++
+  compilation_command = f'g++ eigen_types_iofile_test.cpp -o my_cpp_program'
+  subprocess.run(compilation_command, shell=True, check=True)
 
-# Ejecuta el programa C++
-cpp_program = './metodopotenciadeflacion
-subprocess.run(cpp_program, shell=True)
+  # Ejecuta el programa C++
+  cpp_program = './my_cpp_program'
+  subprocess.run(cpp_program, shell=True)
 
-autovalores = np.loadtxt('autovalores.txt')
-autovectores = np.loadtxt('autovectores.txt')
+  autovalores = np.loadtxt('autovalores.txt')
+  autovectores = np.loadtxt('autovectores.txt')
 
+A = np.array([[2, 1, 4], [1, 3, 0], [4, 0, 4]], dtype=np.float64)
+potenciadeflacion(A)
